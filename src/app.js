9c1c234afa9f33253f282c3fdc071a6b02790702
Abhishek Singh <abhisheksingh7254@gmail.com>
2025-09-25 14:15:08 +0530

    error handling using expressjs(err,req,res,next)

--- src/app.js
+++ src/app.js
@@ -3,16 +3,24 @@ const express = require("express");
 const {AdminAuth}=require("./middleware/auth.js");
 const app=express();
 
-app.use("/admin",AdminAuth);
+//genereal way ro hande error
+app.get("/userdata",(req,res)=>{
+    try{
+        throw new Error("xyzssdfhjf");
+        res.send("userdata sent");
+        
+    }catch(err){
+        res.status(500).send("Something went wrong");
 
-app.use("/user",AdminAuth,(req,res)=>{  //or, we can create a UserAuth same as admin
-    res.send("user is verified");
-});
+    }
+})
 
-app.get("/admin/data",(req,res)=>{
-    res.send("Data has been sent");
-});
-
+//how to handle error using express.always write just above app.listen
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(500).send("Something went wrong");
+    }
+})
 app.listen(7777,()=>{
     console.log("server is running on 7777");
     
