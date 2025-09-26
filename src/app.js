6f4181cf4c13dbe7bda06632fa42a9edd58f4484
Abhishek Singh <abhisheksingh7254@gmail.com>
2025-09-26 15:33:52 +0530

    post API,data added to database

--- src/app.js
+++ src/app.js
@@ -1,13 +1,31 @@
 const express = require("express");
-const {connectDB}= require("./config/database");
+const { connectDB } = require("./config/database");
 const app = express();
+const User = require("./models/user");
 
-connectDB().then(()=>{
-    console.log("Database is connected");
-    app.listen(7777, () => {
-      console.log("server is running on 7777");
-    });
-    
-}).catch((err)=>{
-    console.error("Databse cant be connected");
+//signup API
+app.post("/signup", async (req, res) => {
+  const users = new User({
+    firstName: "Abhishek",
+    lastName: "Singh",
+    emailID: "abhishek@123",
+    password: "abhishek123",
+  });
+  try {
+    await users.save();
+    res.send("Data is added to dabase");
+  } catch (error) {
+    res.status(400).send("Something went wrong:" + error.message);
+  }
 });
+
+connectDB()
+  .then(() => {
+    console.log("database is connected");
+    app.listen(7777, () => {
+      console.log("server is live on port 7777");
+    });
+  })
+  .catch((err) => {
+    console.error("Something went wrong,can't connect to database");
+  });
--- src/models/user.js
+++ src/models/user.js
@@ -1,3 +1,30 @@
+// const mongoose=require("mongoose");
+
+// const userSchema=new mongoose.Schema({
+//     firstName:{
+//         type:String
+//     },
+//     lastName:{
+//         type:String
+//     },
+//     emailID:{
+//         type:String
+//     },
+//     password:{
+//         type:String
+//     },
+//     age:{
+//         type:Number
+//     },
+//     gender:{
+//         type:String
+//     }
+
+// });
+
+// const User=mongoose.model("User",userSchema);
+// module.exports=User;
+
 const mongoose=require("mongoose");
 
 const userSchema=new mongoose.Schema({
@@ -12,15 +39,9 @@ const userSchema=new mongoose.Schema({
     },
     password:{
         type:String
-    },
-    age:{
-        type:Number
-    },
-    gender:{
-        type:String
     }
-
 });
 
+//crating model now
 const User=mongoose.model("User",userSchema);
 module.exports=User;
